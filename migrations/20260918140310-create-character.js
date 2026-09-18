0e9552d47793d30eac61813bdaafdd4005299549
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-09-18 12:58:42 -0300

    feat(migrations) add migrations

---
+++ migrations/20260918140310-create-character.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Characters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      is_founder: {
+        type: Sequelize.BOOLEAN
+      },
+      house: {
+        type: Sequelize.STRING
+      },
+      wand_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Characters');
+  }
+};
